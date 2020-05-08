68bd745820c7e5a9efdee171af85eab8e6d9b217
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2020-05-09 00:20:22 +0200

    chap-7.5

---
+++ post-app/Post.elm
@@ -0,0 +1,35 @@
+module Post exposing (Post, postDecoder, postsDecoder, PostId, idToString)
+
+import Json.Decode as Decode exposing (Decoder, int, list, string)
+import Json.Decode.Pipeline exposing (required)
+
+type PostId = PostId Int
+
+type alias Post =
+    { id : PostId
+    , title : String
+    , authorName : String
+    , authorUrl : String
+    }
+
+
+postsDecoder : Decoder (List Post)
+postsDecoder =
+    list postDecoder
+
+
+postDecoder : Decoder Post
+postDecoder =
+    Decode.succeed Post
+        |> required "id" idDecoder
+        |> required "title" string
+        |> required "authorName" string
+        |> required "authorUrl" string
+
+idDecoder : Decoder PostId
+idDecoder =
+    Decode.map PostId int
+
+idToString : PostId -> String
+idToString (PostId id) =
+    String.fromInt id
